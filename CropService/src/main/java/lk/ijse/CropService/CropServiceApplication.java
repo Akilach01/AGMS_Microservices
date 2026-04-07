52b7f29b179cc7ebc5dba86073acc929c42f420d
Akilach01 <akilapeiris92@gmail.com>
2026-04-07 14:28:44 +0530

    cropservice setup

---
+++ CropService/src/main/java/lk/ijse/CropService/CropServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.CropService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropServiceApplication.class, args);
+	}
+
+}
---
+++ CropService/src/test/java/lk/ijse/CropService/CropServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.CropService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
