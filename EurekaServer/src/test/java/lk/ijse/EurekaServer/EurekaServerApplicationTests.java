6daac6228981fed2d11dbc8b9fe53ce7d7722653
Akilach01 <akilapeiris92@gmail.com>
2026-04-07 14:01:53 +0530

    started eurekaserver setup

---
+++ EurekaServer/src/test/java/lk/ijse/EurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
