795d45bf89540aaa2c331dc58530318b92f46e56
Akilach01 <akilapeiris92@gmail.com>
2026-04-10 14:38:55 +0530

    implementing zone service

---
+++ ZoneService/src/main/java/lk/ijse/ZoneService/service/ZoneService.java
@@ -0,0 +1,35 @@
+package lk.ijse.ZoneService.service;
+
+import lk.ijse.ZoneService.dto.DeviceRegistrationRequest;
+import lk.ijse.ZoneService.dto.LoginRequest;
+import lk.ijse.ZoneService.dto.LoginResponse;
+import lk.ijse.ZoneService.entity.Zone;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
+
+public class ZoneService {
+
+    @Autowired
+    private ZoneRepository zoneRepository;
+
+    @Autowired
+    private IotExternalClient iotExternalClient;
+
+    public Zone createZone(Zone zone) {
+        if (zone.getMinTemp() >= zone.getMaxTemp()){
+            throw new IllegalArgumentException("min temp must be less than max temp");
+        }
+
+        LoginResponse loginResponse = iotExternalClient.login(new LoginRequest("akila","1234"));
+        String token = "Bearer "+loginResponse.getAccessToken();
+
+        DeviceRegistrationRequest deviceRegistrationRequest = new DeviceRegistrationRequest(zone.getName()+ "-Sensor",zone.getName());
+        Map<String,Object> deviceResponse = iotExternalClient.registerDevice(token,deviceRegistrationRequest);
+
+        String externalDeviceId = (String) deviceResponse.get("deviceId");
+        zone.setDeviceId(externalDeviceId);
+
+        return zoneRepository.save(zone);
+    }
+}
