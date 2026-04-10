b8887392d1ff829a92014b7bfed0fcfe422b3eb9
Akilach01 <akilapeiris92@gmail.com>
2026-04-10 14:43:27 +0530

    added zonerepository

---
+++ ZoneService/src/main/java/lk/ijse/ZoneService/repository/ZoneRepository.java
@@ -0,0 +1,8 @@
+package lk.ijse.ZoneService.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ZoneRepository extends JpaRepository<Zone,Long> {
+}
