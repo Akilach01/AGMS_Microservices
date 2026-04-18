b47e72dc90c085a98d9e501af6047bf340d877b4
Akilach01 <akilapeiris92@gmail.com>
2026-04-18 19:36:20 +0530

    zonerepo updated

--- Microservices/ZoneService/src/main/java/lk/ijse/ZoneService/repository/ZoneRepository.java
+++ Microservices/ZoneService/src/main/java/lk/ijse/ZoneService/repository/ZoneRepository.java
@@ -1,8 +1,14 @@
 package lk.ijse.ZoneService.repository;
 
+import lk.ijse.ZoneService.entity.Zone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ZoneRepository extends JpaRepository<Zone,Long> {
+public interface ZoneRepository extends JpaRepository<Zone, Integer> {
+    List<Zone> findByNameContainingIgnoreCase(String name);
+
+    List<Zone> findByMinTempLessThanEqualAndMaxTempGreaterThanEqual(Double min, Double max);
 }
