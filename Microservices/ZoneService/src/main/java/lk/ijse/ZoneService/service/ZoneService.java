5b0954e0727982ec1b555ea3135af1fc928dc7dd
Akilach01 <akilapeiris92@gmail.com>
2026-04-18 19:48:41 +0530

    zoneservice updated

--- Microservices/ZoneService/src/main/java/lk/ijse/ZoneService/service/ZoneService.java
+++ Microservices/ZoneService/src/main/java/lk/ijse/ZoneService/service/ZoneService.java
@@ -1,36 +1,20 @@
 package lk.ijse.ZoneService.service;
 
-import lk.ijse.ZoneService.dto.DeviceRegistrationRequest;
-import lk.ijse.ZoneService.dto.LoginRequest;
-import lk.ijse.ZoneService.dto.LoginResponse;
-import lk.ijse.ZoneService.entity.Zone;
-import lk.ijse.ZoneService.repository.ZoneRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lk.ijse.ZoneService.dto.ZoneDto;
 
-import java.util.Map;
+import java.util.List;
 
-public class ZoneService {
+public interface ZoneService {
 
-    @Autowired
-    private ZoneRepository zoneRepository;
+  ZoneDto save(ZoneDto dto);
+  ZoneDto update(int id, ZoneDto dto);
+  List<ZoneDto> getAll();
+  ZoneDto getById(int id);
+  void delete(int id);
+  String checkZoneStatus(int id, double currentTemp);
+  List<ZoneDto> searchByName(String name);
+    List<ZoneDto> filterByTemperature(double temp);
+    List<String> getAllDeviceIds();
+    long countZones();
 
-    @Autowired
-    private IotExternalClient iotExternalClient;
-
-    public Zone createZone(Zone zone) {
-        if (zone.getMinTemp() >= zone.getMaxTemp()){
-            throw new IllegalArgumentException("min temp must be less than max temp");
-        }
-
-        LoginResponse loginResponse = iotExternalClient.login(new LoginRequest("akila","1234"));
-        String token = "Bearer "+loginResponse.getAccessToken();
-
-        DeviceRegistrationRequest deviceRegistrationRequest = new DeviceRegistrationRequest(zone.getName()+ "-Sensor",zone.getName());
-        Map<String,Object> deviceResponse = iotExternalClient.registerDevice(token,deviceRegistrationRequest);
-
-        String externalDeviceId = (String) deviceResponse.get("deviceId");
-        zone.setDeviceId(externalDeviceId);
-
-        return zoneRepository.save(zone);
-    }
 }
